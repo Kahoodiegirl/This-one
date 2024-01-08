2541930fdbc5343c5ca38598e674a005653113a6
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2024-01-08 16:46:51 +0000

    Hello

---
+++ 4number/Number.java
@@ -0,0 +1,10 @@
+class squares
+{
+    public static void main (String args[])
+    {
+        int num1 = 7;
+        int ans = (num1 * num1) / 2;
+
+        System.out.println(ans);
+    }
+}
---
+++ 4number/together.java
@@ -0,0 +1,17 @@
+class together
+{
+    public static void main (String args[])
+    {
+        int num1, num2, num3, num4;
+        int ans;
+
+        num1 = 6;
+        num2 = 7;
+        num3 = 2;
+        num4 = 20;
+        
+        ans = (num1 + num2+ num3 + num4) / 4;
+
+        System.out.println(ans);
+    }
+}
