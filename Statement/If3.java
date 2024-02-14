98d05eb21e74f936f4146c3bb18a66612014294b
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2024-02-14 16:07:50 +0000

    number grade

---
+++ Statement/If3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main{
+
+public static void main(String[]args)
+{
+Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    
+    System.out.println("Enter a number: ");
+    int number = scanner.nextInt();
+if (number > -1 && number < 69){
+    System.out.println("you get an F");
+}
+
+if (number >69 && number < 74){
+    System.out.println("you get a D");
+}
+
+if (number > 74 && number < 79){
+    System.out.println("you get a c");
+}
+
+if (number > 79 && number < 89){
+    System.out.println("You get a B");
+}
+
+else;
+    if (number > 89 && number < 100){
+        System.out.println("you get an A!! Congratulations");
+    }
+    
+}
+}
