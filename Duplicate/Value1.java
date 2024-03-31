e88e017fefe5c447c93daa6d83d0b71693aa63de
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2024-03-31 23:17:14 +0000

    giy9ijgu6rytjuyih8u

--- Duplicate/Value1.java
+++ Duplicate/Value1.java
@@ -4,7 +4,7 @@ public class  Duplicate{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Allow user to enter up to 25 values
+       
         System.out.println("Enter up to 25 values (enter -1 to stop):");
         int[] values = new int[25];
         int count = 0;
@@ -15,14 +15,14 @@ public class  Duplicate{
             value = scanner.nextInt();
             
             if (value == -1) {
-                break; // Stop entering values if -1 is entered
+                break;
             }
             
             values[count] = value;
             count++;
         }
         
-        // Check for duplicates
+       
         boolean hasDuplicates = false;
         int firstDuplicate = -1;
         int[] duplicatesCount = new int[25];
@@ -39,12 +39,12 @@ public class  Duplicate{
             }
         }
         
-        // Output results
+       
         if (hasDuplicates) {
             System.out.println("Duplicates found!");
             System.out.println("First duplicated value: " + firstDuplicate);
             
-            // Extension: Display how many times each value was duplicated
+            
             System.out.println("Duplicate counts:");
             for (int i = 0; i < duplicatesCount.length; i++) {
                 if (duplicatesCount[i] > 0) {
